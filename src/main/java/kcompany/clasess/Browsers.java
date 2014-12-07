0ff9993c08dae62d0e24c7dee72fc774fda848c0
Konst <kostyan.vk@gmail.com>
2014-12-08 00:47:46 +0200

    browsers

---
+++ src/main/java/kcompany/clasess/Browsers.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kcompany.clasess;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Konst
+ */
+public class Browsers {
+
+    public Browsers() throws IOException {
+
+       
+        
+    }
+
+}
