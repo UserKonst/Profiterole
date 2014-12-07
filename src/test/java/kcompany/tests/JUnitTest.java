73971bdb0e185a7b33b37ef0db70c02aefd63265
Konst <kostyan.vk@gmail.com>
2014-12-07 21:40:49 +0200

    add testMethod findAllSnacksPerCuisine

--- src/test/java/kcompany/tests/JUnitTest.java
+++ src/test/java/kcompany/tests/JUnitTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 import kcompany.pages.CuisinePage;
 import kcompany.pages.MainPage;
 import kcompany.pages.MenuPage;
-import kcompany.pages.RecipePage;
+import kcompany.pages.AllOfRecipesPage;
 import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -25,86 +25,88 @@ import org.openqa.selenium.firefox.FirefoxProfile;
  * @author Konst
  */
 public class JUnitTest {
-
-    public JUnitTest() {
-    }
-
-    public WebDriver driver;
-    public FirefoxProfile profile;
-
-    @Ignore
-    @Test
-    public void should_Display_SirnyePalochki_Selected_From_Day_Menu() throws InterruptedException {
-
-        // Open main page
-        MainPage mainPage = new MainPage(driver);
-        mainPage.getMainPage(driver);
-
-        // Select menu
-        MenuPage menuPage = mainPage.selectMenu("Меню на день");
-
-        // Select recipe
-        RecipePage recipePage = menuPage.selectRecipe("Ужин");
-
-        // Select cuisine
-        CuisinePage cuisinePage = recipePage.selectCuisine("Французская");
-        cuisinePage.selectMenu("Закуски");
-
-        // Select dish
-        String dish = "Сырные палочки из пармезана";
-        cuisinePage.addDish(dish);
-
-        assertTrue("Выбранное блюдо не отображается в меню", menuPage.isDishPresent(dish));
-
-    }
-
-    @Ignore
-    @Test
-    public void should_Display_SirnyePalochki_Selected_From_Week_Menu() throws InterruptedException {
-
-        // Open main page
-        MainPage mainPage = new MainPage(driver);
-        mainPage.getMainPage(driver);
-
-        // Select menu
-        MenuPage menuPage = mainPage.selectMenu("Меню на неделю");
-
-        // Select day
-        menuPage.selectDay("Вт");
-
-        // Select recipe
-        RecipePage recipePage = menuPage.selectRecipe("Обед");
-
-        // Select cuisine
-        CuisinePage cuisinePage = recipePage.selectCuisine("Французская");
-        cuisinePage.selectMenu("Закуски");
-
-        // Select dish
-        String dish = "Сырные палочки из пармезана";
-        cuisinePage.addDish(dish);
-
-        assertTrue("Выбранное блюдо не отображается в меню", menuPage.isDishPresent(dish));
-
-    }
-
-    @Before
-    public void setUp() {
-
-//        File file = new File("D:\\Fprofile");
-//        profile = new FirefoxProfile(file);
-        driver = new FirefoxDriver();
-
-        // Timeouts
-        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-    }
-
-    @After
-    public void tearDown() {
-        driver.manage().deleteAllCookies();
-        driver.quit();
-
-    }
+//
+//    public JUnitTest() {
+//    }
+//
+//    public WebDriver driver;
+//    public FirefoxProfile profile;
+//
+//  
+//    @Test
+//    public void should_Display_SirnyePalochki_Selected_From_Day_Menu() throws InterruptedException {
+//
+//        // Open main page
+//        MainPage mainPage = new MainPage(driver);
+//        mainPage.getMainPage(driver);
+//
+//        
+//        
+////        // Select menu
+////        MenuPage menuPage = mainPage.selectMenu("Меню на день");
+////
+////        // Select recipe
+////        RecipePage recipePage = menuPage.selectRecipe("Ужин");
+////
+////        // Select cuisine
+////        CuisinePage cuisinePage = recipePage.selectCuisine("Французская");
+////        cuisinePage.selectMenu("Закуски");
+////
+////        // Select dish
+////        String dish = "Сырные палочки из пармезана";
+////        cuisinePage.addDish(dish);
+////
+////        assertTrue("Выбранное блюдо не отображается в меню", menuPage.isDishPresent(dish));
+//
+//    }
+//
+//    @Ignore
+//    @Test
+//    public void should_Display_SirnyePalochki_Selected_From_Week_Menu() throws InterruptedException {
+//
+//        // Open main page
+//        MainPage mainPage = new MainPage(driver);
+//        mainPage.getMainPage(driver);
+//
+//        // Select menu
+//       // MenuPage menuPage = mainPage.selectMenu("Меню на неделю");
+//
+//        // Select day
+//        menuPage.selectDay("Вт");
+//
+//        // Select recipe
+//        AllOfRecipesPage recipePage = menuPage.selectRecipe("Обед");
+//
+//        // Select cuisine
+//        CuisinePage cuisinePage = recipePage.selectCuisine("Французская");
+//        cuisinePage.selectMenu("Закуски");
+//
+//        // Select dish
+//        String dish = "Сырные палочки из пармезана";
+//        cuisinePage.addDish(dish);
+//
+//        assertTrue("Выбранное блюдо не отображается в меню", menuPage.isDishPresent(dish));
+//
+//    }
+//
+//    @Before
+//    public void setUp() {
+//
+////        File file = new File("D:\\Fprofile");
+////        profile = new FirefoxProfile(file);
+//        driver = new FirefoxDriver();
+//
+//        // Timeouts
+//        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        driver.manage().window().maximize();
+//    }
+//
+//    @After
+//    public void tearDown() {
+//        driver.manage().deleteAllCookies();
+//        driver.quit();
+//
+//    }
 
 }
--- src/test/java/kcompany/tests/SampleTest.java
+++ src/test/java/kcompany/tests/SampleTest.java
@@ -5,13 +5,21 @@
  */
 package kcompany.tests;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import kcompany.pages.html.SearchPage;
+import kcompany.clasess.Cuisine;
+import kcompany.pages.AllOfRecipesPage;
+import kcompany.pages.MainPage;
 import org.junit.After;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 
 /**
  *
@@ -19,29 +27,75 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class SampleTest {
 
-    private final WebDriver driver = new FirefoxDriver();
-    private final SearchPage searchPage = new SearchPage(driver);
+    private WebDriver driver; //= new FirefoxDriver();
+    public FirefoxProfile profile;
 
     @Before
     public void loadPage() {
+
+        File file = new File("D:\\Fprofile");
+        profile = new FirefoxProfile(file);
+        driver = new FirefoxDriver(profile);
+
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("http://gioia-profiterole.rhcloud.com");
+
     }
 
     @Test
-    public void sampleTest() {
-        searchPage.getAllRecipes();
+    public void AllOfRecipesPage_Should_Contains_12_Cuisines() throws InterruptedException {
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.getMainPage();
+
+        AllOfRecipesPage recipesPage;
+        recipesPage = mainPage.getAllOfRecipesPage();
+        Set<Cuisine> setCuisine = recipesPage.findAllCousines();
+
+        assertEquals("Amount of cuisines isn't 12", 12, setCuisine.size());
     }
 
     @Test
-    public void sampleTest1() {
-        searchPage.getMenuForDay();
-    }
+    public void Ukrainian_Cuisine_Should_Contains_5_Snacks() {
 
+        MainPage mainPage = new MainPage(driver);
+        mainPage.getMainPage();
+        AllOfRecipesPage recipesPage = mainPage.getAllOfRecipesPage();
+
+        Map<Cuisine, List<String>> mapRecipes;
+        mapRecipes = recipesPage.findAllSnacksPerCousine();
+
+        int ukrSnacks = mapRecipes.get(new Cuisine("Украинская")).size();
+        assertEquals("Ukrainian cuisine doesn't have 5 snacks", 5, ukrSnacks);
+
+    }
+    
+    @Test
+    public void AllOfRecipesPage_Should_Contains_16_Snacks() {
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.getMainPage();
+        AllOfRecipesPage recipesPage = mainPage.getAllOfRecipesPage();
+
+        Map<Cuisine, List<String>> mapRecipes;
+        mapRecipes = recipesPage.findAllSnacksPerCousine();
+
+        int allSnacks = 0;
+        
+        for (Map.Entry<Cuisine, List<String>> entrySet : mapRecipes.entrySet()) {
+            //Cuisine key = entrySet.getKey();
+            List<String> value = entrySet.getValue();
+            allSnacks = allSnacks + value.size();
+        }
+             
+        assertEquals("All cuisines don't have 16 snacks", 16, allSnacks);
+
+    }
+    
     @After
     public void closeDriver() {
+        driver.manage().deleteAllCookies();
         driver.quit();
     }
 }
